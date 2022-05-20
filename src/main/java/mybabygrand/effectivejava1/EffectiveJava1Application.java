c550d8044d8d48af309996e842d7727287db49c4
mybabygrand <amuro_ray@kakao.com>
2022-05-20 23:05:58 +0900

    init

---
+++ src/main/java/mybabygrand/effectivejava1/EffectiveJava1Application.java
@@ -0,0 +1,13 @@
+package mybabygrand.effectivejava1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveJava1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EffectiveJava1Application.class, args);
+    }
+
+}
---
+++ src/test/java/mybabygrand/effectivejava1/EffectiveJava1ApplicationTests.java
@@ -0,0 +1,13 @@
+package mybabygrand.effectivejava1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveJava1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
